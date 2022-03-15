bb81f06e0190960df1b530cce530b363b7160d8c
ANTAJUGA <62303168+ANTAJUGA@users.noreply.github.com>
2022-03-15 09:26:45 -0500

    Add files via upload

---
+++ views/template/MenuDespegable.php
@@ -0,0 +1,127 @@
+<div id="sidebar" class="sidebar                  responsive                    ace-save-state">
+				<script type="text/javascript">
+					try{ace.settings.loadState('sidebar')}catch(e){}
+				</script>
+
+				<ul class="nav nav-list">
+
+					<li class="nav-item"    id="inicio">
+						<a href="inicio.php">
+							<i class="menu-icon fa fa-home"></i>
+							<span class="menu-text"> Inicio</span>
+						</a>
+						<b class="arrow"></b>
+					</li>
+
+					<li class="nav-item" >
+						<a href="#" class="dropdown-toggle">
+                        <i class=" menu-icon fa fa-users"></i>
+							<span class="menu-text">
+								Adherentes
+							</span>
+
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+
+						<b class="arrow"></b>
+						<ul class="submenu">
+                            <li class="nav-item"   id="adherenteView">
+                                <a href="adherenteView.php">
+                                    <i class="menu-icon fa fa-hdd-o"></i>
+                                    <span class="menu-text">Insertar Adherente</span>
+                                </a>
+                                <b class="arrow"></b>
+                            </li>
+							<li class="nav-item" id="guiaView">
+								<a href="guiaView.php">
+									<i class="menu-icon fa  fa-flag"></i>
+									<span class="menu-text">Listar Registro</span>
+								</a>
+								<b class="arrow"></b>
+							</li>
+							<li class="nav-item" id="">
+								<a href="#">
+									<i class="menu-icon fa  fa-folder-open-o"></i>
+									<span class="menu-text">Lista Total</span>
+								</a>
+								<b class="arrow"></b>
+							</li>
+							<li class="nav-item" id="">
+								<a href="#">
+									<i class="menu-icon glyphicon glyphicon-pencil"></i>
+									<span class="menu-text">Registro Electoral</span>
+								</a>
+								<b class="arrow"></b>
+							</li>
+						</ul>
+
+					</li>
+                    <li class="nav-item" >
+						<a href="#" class="dropdown-toggle">
+                        <i class="menu-icon glyphicon  glyphicon-inbox"></i>
+							<span class="menu-text">
+								Catalogo
+							</span>
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+						<b class="arrow"></b>
+						<ul class="submenu">
+                            <li class="nav-item"   id="imprimirPadronesView">
+                                <a href="imprimirPadronesView.php">
+                                    <i class="menu-icon glyphicon glyphicon-print"></i>
+                                    <span class="menu-text">Padrónes</span>
+                                </a>
+                                <b class="arrow"></b>
+                            </li>
+							<li class="nav-item" id="">
+								<a href="#">
+									<i class="menu-icon fa  fa-book"></i>
+									<span class="menu-text">Certificado</span>
+								</a>
+								<b class="arrow"></b>
+							</li>
+						</ul>
+
+					</li>
+					<li class="nav-item" >
+						<a href="#" class="dropdown-toggle">
+                        <i class="menu-icon fa fa-usd"></i>
+							<span class="menu-text">
+								Aportes
+							</span>
+							<b class="arrow fa fa-angle-down"></b>
+						</a>
+						<b class="arrow"></b>
+						<ul class="submenu">
+                            <li class="nav-item"   id="">
+                                <a href="#">
+                                    <i class="menu-icon fa fa-money"></i>
+                                    <span class="menu-text">Adherentes</span>
+                                </a>
+                                <b class="arrow"></b>
+                            </li>
+							<li class="nav-item" id="">
+								<a href="#">
+									<i class="menu-icon fa fa-calendar"></i>
+									<span class="menu-text">Reportes</span>
+								</a>
+								<b class="arrow"></b>
+							</li>
+						</ul>
+
+					</li>
+					<?php if($_SESSION['tipo_usuario']==1){ ?>
+                    <li class="nav-item" id="crearUsuario">
+						<a href="crearUsuario.php">
+							<i class="menu-icon glyphicon glyphicon-user"></i>
+							<span class="menu-text"> Usuario</span>
+						</a>
+						<b class="arrow"></b>
+					</li>
+					<?php } ?>
+				</ul><!-- /.nav-list -->
+
+				<div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
+					<i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+				</div>
+			</div>
---
+++ views/template/navbar.php
@@ -0,0 +1,71 @@
+<?php 
+$id = $_SESSION['id'];//importante
+$persona_Id = $_SESSION['persona_Id'];//importante
+$objeto = new Persona();
+$usuario = $objeto->Obtener_Datos('padronp','id',$persona_Id,$conexion);
+$avatar= $objeto->Obtener_Datos('usuario','id',$id,$conexion);
+?>
+<div id="navbar" class="navbar navbar-default          ace-save-state">
+			<div class="navbar-container ace-save-state" id="navbar-container">
+                <!-- boton toggle para mostrar el menu -->
+				<button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar">
+					<span class="sr-only">Toggle sidebar</span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+				</button>
+                <!-- texto de cabecera del instituto -->
+				<div class="navbar-header pull-left">
+					<a href="../views/inicio.php" class="navbar-brand">
+						<small>
+							<i class="glyphicon glyphicon-grain"></i>
+							MUPP Listas - 18
+						</small>
+					</a>
+				</div>
+
+				<div class="navbar-buttons navbar-header pull-right" role="navigation">
+					<ul class="nav ace-nav">
+                        <!-- Perfil del bibliotecario -->
+						<li class="light-blue dropdown-modal">
+							<a data-toggle="dropdown" href="#" class="dropdown-toggle">
+								<img class="nav-user-photo" src="" />
+								<span class="">
+									<?php echo $usuario['apellido_nombre']; ?>
+								</span>
+
+								<i class="ace-icon fa fa-caret-down"></i>
+							</a>
+
+							<ul class="user-menu dropdown-menu-right dropdown-menu dropdown-yellow dropdown-caret dropdown-close">
+								<li>
+									<a href="perfilView.php">
+										<i class="ace-icon fa fa-cog"></i>
+										Settings
+									</a>
+								</li>
+
+								<li>
+									<a href="#">
+										<i class="ace-icon fa fa-user"></i>
+										Profile
+									</a>
+								</li>
+
+								<li class="divider"></li>
+
+								<li>
+									<a href="../controllers/controladorCerrarSesion.php">
+										<i class="ace-icon fa fa-power-off"></i>
+										Logout
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div><!-- /.navbar-container -->
+		</div>
